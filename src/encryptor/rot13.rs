24b3839bc7e23d379290a7cc3532c4b302385fac
Daniel Tinivella <dtinivella@gmail.com>
2023-08-06 14:45:37 -0300

    initial commit

---
+++ src/encryptor/rot13.rs
@@ -0,0 +1,12 @@
+//
+// rot13.rs
+// Copyright (C) 2023 tinix <tinix@arch>
+// Distributed under terms of the MIT license.
+//
+
+
+pub struct Rot13(pub String);
+
+impl super::Encryptable for Rot13 {
+
+}
